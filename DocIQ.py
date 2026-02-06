67d2681ba7707ea1ce851ec07199a1b6c19f028b
Azeez Akintonde <treadwayanalytics@gmail.com>
2026-02-06 16:01:28 +0100

    second push

--- DocIQ.py
+++ DocIQ.py
@@ -8,17 +8,17 @@ records = []
 for file in folder_path.iterdir():
     if file.suffix.lower() in [".jpg", ".jpeg", ".png", ".pdf"]:
 
-        # 1. Send file to Document Intelligence
+
         with open(file, "rb") as f:
             poller = client.begin_analyze_document(
-                model_id=model_id,   # e.g. "prebuilt-read"
+                model_id=model_id,  
                 document=f
             )
 
-        # 2. Wait for OCR to complete
+
         result = poller.result()
 
-        # 3. Extract text
+
         lines = []
         for page in result.pages:
             for line in page.lines:
@@ -26,14 +26,14 @@ for file in folder_path.iterdir():
 
         extracted_text = "\n".join(lines)
 
-        # 4. Store result (one row per file)
+     
         records.append({
             "file_name": file.name,
             "file_type": file.suffix.lower(),
             "content": extracted_text
         })
 
-# 5. Display results for validation
+
 for r in records:
     print(f"\n===== {r['file_name']} =====")
     print(r["content"][:1000])
